996a478219accb69484e85215de792b2b759d5eb
Travis Peterson <tpeterson@acomsolutions.com>
2019-04-09 12:15:45 -0700

    remove pointless css

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,6 +3,6 @@ import Container from 'reactstrap/lib/Container';
 
 export default class Layout extends React.Component{
     render() {
-        return <Container fluid className='p-0 pt-5'>{this.props.children}</Container>
+        return <Container fluid>{this.props.children}</Container>
     }
 }
